200f12249ebdb34f079cf1c743afe076e8288133
Tim Lee <systeco@hotmail.com>
2022-10-28 15:29:43 -0400

    1028-11

--- components/customitem.js
+++ components/customitem.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function CustomItemScreen(props) {
   return (
     <div>
-      <div className="flex justify-center ">
+      <div className="flex justify-center card ">
         <div>
           <Image
             src={props.image}
@@ -15,11 +15,13 @@ export default function CustomItemScreen(props) {
           />
         </div>
       </div>
-      <div>{props.productname}</div>
-      <div>{props.price}</div>
-      <div>{props.description}</div>
-      <div>{props.description1}</div>
-      <div>{props.description2}</div>
+      <div className="bg-slate-100">
+        <div>{props.productname}</div>
+        <div className="font-bold text-lg">U$: {props.price}</div>
+        <div>{props.description}</div>
+        <div className="font-bold">{props.description1}</div>
+        <div>{props.description2}</div>
+      </div>
       {/* <div>{props.user}</div> */}
     </div>
   );
