00245584bbbcbafcfa1b654dcf8667a8c05fb3e4
Tim Lee <systeco@hotmail.com>
2022-10-28 12:20:14 -0400

    1028-8

--- pages/unauthorized.js
+++ pages/unauthorized.js
@@ -1,16 +1,16 @@
-import { useRouter } from 'next/router'
-import React from 'react'
-import Layout from '../components/Layout'
+import { useRouter } from 'next/router';
+import React from 'react';
+import Layout from '../components/layout';
 
 export default function Unauthorized() {
-        const router = useRouter();
-        const { message } = router.query;
+  const router = useRouter();
+  const { message } = router.query;
   return (
     <div>
-        <Layout title='Unauthorized Page'>
-                <h1 className='text-xl'>Access Denied</h1>
-                {message && <div className='mb-4 text-red-500'>{message}</div> }
-        </Layout>
+      <Layout title="Unauthorized Page">
+        <h1 className="text-xl">Access Denied</h1>
+        {message && <div className="mb-4 text-red-500">{message}</div>}
+      </Layout>
     </div>
-  )
+  );
 }
